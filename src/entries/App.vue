bb27cb089107b88ff465c9fa8239323cce2da2d3
small752 <small752@163.com>
2018-11-12 08:09:27 +0800

    feat: 公共文件

---
+++ src/entries/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/1px.less';
+</style>
+
+<style>
+html, body, #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-overflow-scrolling: touch;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.icon {
+  font-family: 'iconfont';
+  font-style: inherit;
+  width: 1em;
+  height: 1em;
+}
+
+p {
+  margin: 0;
+}
+</style>
